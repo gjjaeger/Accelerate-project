714aba91ba917224f8041ce13036f38cfdbd9792
gjjaeger <gabrieljaeger@acquia-drupal-7-41-46-6284.dd127.0.0.1>
2017-02-04 14:06:36 +0800

    the moment when i had trouble with javascript
    can’t get javascript to be recognized by the site.

---
+++ gabrieljaeger.js
@@ -0,0 +1,13 @@
+
+/*(document).ready(function(){
+	console.log( "ready!" );
+    $('body').scrollspy({target: ".navbar", offset: 50});   
+});*/
+
+  $(document).ready(function(){
+    console.log( "ready!" );
+    $("#btn").on("click",function(){
+      
+      alert("jfhelkf");
+    });
+  });
